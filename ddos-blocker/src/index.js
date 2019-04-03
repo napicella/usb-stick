ce28f57b889eadd213173b72c65aa6ff043551fa
napicell <napicell@amazon.com>
2019-04-03 20:09:23 +0300

    Added ddos blocker

--- ddos-blocker/src/index.js
+++ ddos-blocker/src/index.js
@@ -1,30 +1,35 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/APIGateway.html#createApiKey-property
+// https://docs.aws.amazon.com/cli/latest/reference/apigateway/update-stage.html
+// aws apigateway update-stage --rest-api-id rrtvx1muac --stage-name 'Prod' --patch-operations op=replace,path=/~1*/*/throttling/rateLimit,value=1
 const apigateway = new AWS.APIGateway({apiVersion: '2015-07-09', region: process.env.AWS_REGION});
 
-
 const handler = (event, context, callback) => {
-    const createKeyParams = {
-        customerId: 'tooManyCalls',
-        description: 'tooManyCalls',
-        enabled: true || false,
-        generateDistinctId: true || false,
-        name: 'tooManyCalls',
-        value: 'STRING_VALUE'
+
+    var params = {
+        restApiId: 'rrtvx1muac',
+        stageName: 'Prod',
+        patchOperations: [{
+            op: 'replace',
+            path: '/~1*/*/throttling/rateLimit',
+            value: '0'
+        }]
     };
-    apigateway.createApiKey(createKeyParams, function (err, data) {
+
+    apigateway.updateStage(params, function(err, data) {
         if (err) {
             console.log(err, err.stack);
-            callback(err)
+            callback(err);
         } else {
+            console.log("All the request will be throttled");
             callback(null, {
                 statusCode: 200,
-                body: JSON.stringify({message : "Api key created!"})
+                body: JSON.stringify({message : "All the request will be throttled!!"})
             });
         }
     });
-};
+
+}
 
 module.exports = {
     handler,
