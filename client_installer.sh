722f33695abd55b26679d43135eac9bf586f4258
napicell <napicell@amazon.com>
2019-07-07 13:41:16 +0300

    update installer. Run the script as sudo instead ov single commands

--- client_installer.sh
+++ client_installer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo wget https://github.com/napicella/usb-stick/blob/master/release/usb?raw=true -O /usr/local/bin/usb
-sudo chmod +x /usr/local/bin/usb
+wget https://github.com/napicella/usb-stick/blob/master/release/usb?raw=true -O /usr/local/bin/usb
+chmod +x /usr/local/bin/usb
