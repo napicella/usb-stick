8c1830a0df5c8c83f69657d7fa2583cac0a5c18a
napicell <napicell@amazon.com>
2019-07-07 13:38:16 +0300

    update installer. Sudo permissions are required to set the executable flag

--- client_installer.sh
+++ client_installer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-wget https://github.com/napicella/usb-stick/blob/master/release/usb?raw=true -O /usr/local/bin/usb
-chmod +x /usr/local/bin/usb
+sudo wget https://github.com/napicella/usb-stick/blob/master/release/usb?raw=true -O /usr/local/bin/usb
+sudo chmod +x /usr/local/bin/usb
